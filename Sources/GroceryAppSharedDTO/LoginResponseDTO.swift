c4f2a7b2baa89c859995dae9b9b56f314ef8427d
Rychillie <rychillie@rychillie.net>
2024-11-24 21:31:55 -0300

    register and login response

---
+++ Sources/GroceryAppSharedDTO/LoginResponseDTO.swift
@@ -0,0 +1,22 @@
+//
+//  LoginResponseDTO.swift
+//  GroceryAppSharedDTO
+//
+//  Created by Rychillie Umpierre de Oliveira on 24/11/24.
+//
+
+import Foundation
+
+public struct LoginResponseDTO: Codable {
+    public let error: Bool
+    public var reason: String? = nil
+    public var token: String? = nil
+    public var userId: UUID? = nil
+    
+    public init(error: Bool, reason: String? = nil, token: String? = nil, userId: UUID? = nil) {
+        self.error = error
+        self.reason = reason
+        self.token = token
+        self.userId = userId
+    }
+}
---
+++ Sources/GroceryAppSharedDTO/RegisterResponseDTO.swift
@@ -0,0 +1,18 @@
+//
+//  RegisterResponseDTO.swift
+//  GroceryAppSharedDTO
+//
+//  Created by Rychillie Umpierre de Oliveira on 24/11/24.
+//
+
+import Foundation
+
+public struct RegisterResponseDTO: Codable {
+    public let error: Bool
+    public var reason: String? = nil
+    
+    public init(error: Bool, reason: String? = nil) {
+        self.error = error
+        self.reason = reason
+    }
+}
