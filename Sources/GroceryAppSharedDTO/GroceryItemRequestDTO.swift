138b2e06232ef4c76d6f1de9c14a11a4c548df25
Rychillie <rychillie@rychillie.net>
2024-11-26 14:12:40 -0300

    Add Grocery Item DTO

---
+++ Sources/GroceryAppSharedDTO/GroceryItemRequestDTO.swift
@@ -0,0 +1,22 @@
+//
+//  GroceryItemRequestDTO.swift
+//  GroceryAppSharedDTO
+//
+//  Created by Rychillie Umpierre de Oliveira on 26/11/24.
+//
+
+import Foundation
+
+public struct GroceryItemRequestDTO: Codable {
+    
+    public let title: String
+    public let price: Double
+    public let quantity: Int
+    
+    public init(title: String, price: Double, quantity: Int) {
+        self.title = title
+        self.price = price
+        self.quantity = quantity
+    }
+    
+}
---
+++ Sources/GroceryAppSharedDTO/GroceryItemResponseDTO.swift
@@ -0,0 +1,24 @@
+//
+//  GroceryItemResponseDTO.swift
+//  GroceryAppSharedDTO
+//
+//  Created by Rychillie Umpierre de Oliveira on 26/11/24.
+//
+
+import Foundation
+
+public struct GroceryItemResponseDTO: Codable {
+    
+    public let id: UUID
+    public let title: String
+    public let price: Double
+    public let quantity: Int
+    
+    public init(id: UUID, title: String, price: Double, quantity: Int) {
+        self.id = id
+        self.title = title
+        self.price = price
+        self.quantity = quantity
+    }
+
+}
